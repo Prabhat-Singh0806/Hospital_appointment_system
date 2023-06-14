2e4d235ea949a66f035a95fa79e55e1a5f71610f
Prabhat <Prab8600@gmail.com>
2023-06-14 20:37:07 +0530

    integrated css

--- invoice.php
+++ invoice.php
@@ -74,7 +74,7 @@ else {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>invoice</title>
-    <link rel="stylesheet" type="text/css" href="print.css" media="print">
+    <link rel="stylesheet" type="text/css" href="css/print.css" media="print">
     <style>
         * {
             box-sizing: border-box;
--- patientsample.php
+++ patientsample.php
@@ -21,8 +21,8 @@ while ($num = mysqli_fetch_assoc($result)) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="mainstyle.css" type="text/css">
-  <link rel="stylesheet" href="responsive.css">
+  <link rel="stylesheet" href="css/mainstyle.css" type="text/css">
+  <link rel="stylesheet" href="css/responsive.css">
   <title>Document</title>
   <style>
     .linkbutton{
